14e7d5e537c25c01a97b82fede42422bd0ea3789
Anna Laws <a.laws2@exeter.ac.uk>
2024-04-04 14:14:02 +0100

    :broom: Tidy code on demo page

--- pages/2_Interactive_demo.py
+++ pages/2_Interactive_demo.py
@@ -1,5 +1,5 @@
 """
-Streamlit app template.
+MUSTER results display app.
 
 Because a long app quickly gets out of hand,
 try to keep this document to mostly direct calls to streamlit to write
@@ -26,43 +26,23 @@ import utilities.container_results as results
 # ###########################
 page_setup()
 
-# Title:
-st.markdown('# MUSTER')
-
-
-import streamlit as st
-
-st.markdown('''
-This model shows predicted outcomes for non-large vessel occlusion (nLVO) and large vessel occlusion 
-stroke. Outcomes are calculated for 34,000 small areas (LSOAs) across England based on expected 
-travel times, and other timing parameters chosen by the slider bars on the right.
-
-More detail may be found on estimation of stroke outcomes [here](https://samuel-book.github.io/stroke_outcome/intro.html). 
-The reported outcomes are for treated patients (they do not include patients unsuitable for treatment, 
-haemorrhagic strokes, or mimics)
-
-Three pathways are modelled, through to thrombectomy (note: thrombectomy is only applied to large 
-vessel occlusions; non-large vessel occlusions are treated with thrombolysis only). The three pathways are:
-
-1) *Drip-and-ship*: All patients are taken to their closest emergency stroke unit, all of which 
-provide thrombolysis. For patients who receive thrombectomy there is a transfer to a thrombectomy-capable 
-if the patient has first attended a hopsital that provides thrombolysis only.
-
-2) *Mothership*: All patients are taken to a comprehensive stroke centre that can provide both 
-thrombolysis and thrombectomy.
-
-3) *Mobile stroke unit (MSU)*: MSUs are dispatched, from comprehensive stroke centres, to stroke patients. 
-Head scans and thrombolysis are provided on-scene, where the patient is. For patients who have been 
-treated with thrombolysis or considered suitable for thrombectomy, the MSU takes the patient to the 
-comprehensive stroke centre. Where a patient does not receive thrombolysis, and is not considered 
-a candidate for thrombectomy, the MSU becomes available for another stroke patient, and a standard 
-ambulance conveys the patient to the closest emergency stroke unit. In this particular model there 
-are no capacity limits for the MSU, and it is assumed all strokes are triaged correctly with the 
-emergency call - the model shows outcomes if all patients were seen by a MSU.
-''')
-
-st.image('./pages/images/stroke_treatment.jpg')
-
+# Make containers:
+# +-----------------------+
+# |    container_intro    |
+# +-----------------------+
+# |  container_timeline   |
+# +-----------------------+
+# | container_shared_data |
+# +-----------------------+
+# | container_params_here |
+# +-----------------------+
+# |  container_outcomes   |
+# +-----------------------+
+container_intro = st.container()
+container_timeline = st.container()
+container_shared_data = st.container()
+container_params_here = st.container()
+container_outcomes = st.container()
 
 # ###########################
 # ########## SETUP ##########
@@ -114,57 +94,51 @@ results_dict = dict([(k, results_dict[k]) for k in list(results_dict.keys())
 (times_dicts, times_cum_dicts, times_cum_label_dicts
  ) = build_data_for_timeline(fixed_dict | treatment_time_dict | input_dict)
 
+# Convert results to a DataFrame with multiple column headers.
 # Column header names: occlusion, pathway, treatment, outcome.
 df_results = utils.convert_results_dict_to_multiindex_df(results_dict)
 
-# Make a container for parameters shared across all pathway types.
-# The results will be drawn first but this container appears higher.
-container_shared_data = st.container()
+# ########################################
+# ########## WRITE TO STREAMLIT ##########
+# ########################################
+with container_intro:
+    # Title:
+    st.markdown('# MUSTER')
 
-draw_timeline(times_cum_dicts, times_cum_label_dicts)
+    st.markdown('''
+    This model shows predicted outcomes for non-large vessel occlusion (nLVO) and large vessel occlusion 
+    stroke. Outcomes are calculated for 34,000 small areas (LSOAs) across England based on expected 
+    travel times, and other timing parameters chosen by the slider bars on the right.
 
-st.markdown('''
-### Outcomes ###
+    More detail may be found on estimation of stroke outcomes [here](https://samuel-book.github.io/stroke_outcome/intro.html). 
+    The reported outcomes are for treated patients (they do not include patients unsuitable for treatment, 
+    haemorrhagic strokes, or mimics)
 
-* **mrs_0-2**: Proportion patients modified Rankin Scale 0-2 (higher is better)
-* **mrs_shift**: Average shift in modified Rankin Scale (negative is better)
-* **utility**: Average utility (higher is better)
-* **utility_shift**: Average improvement in (higher is better)
-''')
+    Three pathways are modelled, through to thrombectomy (note: thrombectomy is only applied to large 
+    vessel occlusions; non-large vessel occlusions are treated with thrombolysis only). The three pathways are:
 
-# User inputs for how to display the data:
-group_by = st.radio(
-    'Group results by:',
-    ['Treatment type', 'Outcome type']
-    )
+    1) *Drip-and-ship*: All patients are taken to their closest emergency stroke unit, all of which 
+    provide thrombolysis. For patients who receive thrombectomy there is a transfer to a thrombectomy-capable 
+    if the patient has first attended a hopsital that provides thrombolysis only.
 
-if group_by == 'Treatment type':
-    for stroke_type in ['ivt', 'mt', 'ivt_mt']:
-        df_here = utils.take_subset_by_column_level_values(
-            df_results.copy(), treatment=[stroke_type])
-        df_here = utils.convert_row_to_table(
-            df_here, ['occlusion', 'outcome'])
-        st.markdown(f'### {stroke_type}')
-        style_dict = results.make_column_style_dict(
-            df_here.columns, format='%.3f')
-        st.dataframe(
-            df_here,
-            column_config=style_dict
-            )
-else:
-    for outcome in ['mrs_shift', 'mrs_0-2', 'utility', 'utility_shift']:
-        df_here = utils.take_subset_by_column_level_values(
-            df_results, outcome=[outcome])
-        df_here = utils.convert_row_to_table(
-            df_here, ['occlusion', 'treatment'])
-        st.markdown(f'### {outcome}')
-        style_dict = results.make_column_style_dict(
-            df_here.columns, format='%.3f')
-        st.dataframe(
-            df_here,
-            column_config=style_dict
-            )
+    2) *Mothership*: All patients are taken to a comprehensive stroke centre that can provide both 
+    thrombolysis and thrombectomy.
 
+    3) *Mobile stroke unit (MSU)*: MSUs are dispatched, from comprehensive stroke centres, to stroke patients. 
+    Head scans and thrombolysis are provided on-scene, where the patient is. For patients who have been 
+    treated with thrombolysis or considered suitable for thrombectomy, the MSU takes the patient to the 
+    comprehensive stroke centre. Where a patient does not receive thrombolysis, and is not considered 
+    a candidate for thrombectomy, the MSU becomes available for another stroke patient, and a standard 
+    ambulance conveys the patient to the closest emergency stroke unit. In this particular model there 
+    are no capacity limits for the MSU, and it is assumed all strokes are triaged correctly with the 
+    emergency call - the model shows outcomes if all patients were seen by a MSU.
+    ''')
+
+    st.image('./pages/images/stroke_treatment.jpg')
+
+with container_timeline:
+    st.markdown('### Timeline for this scenario:')
+    draw_timeline(times_cum_dicts, times_cum_label_dicts)
 
 with container_shared_data:
     st.markdown('## Fixed values')
@@ -187,8 +161,12 @@ with container_shared_data:
 
     # Travel times:
     st.markdown('Average travel times (minutes) to closest units')
-    cols = ['nearest_ivt_time', 'nearest_mt_time',
-            'transfer_time', 'nearest_msu_time']
+    cols = [
+        'nearest_ivt_time',
+        'nearest_mt_time',
+        'transfer_time',
+        'nearest_msu_time'
+        ]
     df_travel = pd.Series(dict([(k, fixed_dict[k]) for k in cols]))
     style_dict = results.make_column_style_dict(
         df_travel.index, format='%d')
@@ -198,6 +176,7 @@ with container_shared_data:
         hide_index=True
         )
 
+with container_params_here:
     st.markdown('## This scenario')
 
     st.markdown('### Treatment times ###')
@@ -238,5 +217,47 @@ with container_shared_data:
         # hide_index=True
         )
 
+with container_outcomes:
+    st.markdown('''
+    ### Outcomes ###
+
+    * **mrs_0-2**: Proportion patients modified Rankin Scale 0-2 (higher is better)
+    * **mrs_shift**: Average shift in modified Rankin Scale (negative is better)
+    * **utility**: Average utility (higher is better)
+    * **utility_shift**: Average improvement in (higher is better)
+    ''')
+
+    # User inputs for how to display the data:
+    group_by = st.radio(
+        'Group results by:',
+        ['Treatment type', 'Outcome type']
+        )
+
+    if group_by == 'Treatment type':
+        for stroke_type in ['ivt', 'mt', 'ivt_mt']:
+            df_here = utils.take_subset_by_column_level_values(
+                df_results.copy(), treatment=[stroke_type])
+            df_here = utils.convert_row_to_table(
+                df_here, ['occlusion', 'outcome'])
+            st.markdown(f'### {stroke_type}')
+            style_dict = results.make_column_style_dict(
+                df_here.columns, format='%.3f')
+            st.dataframe(
+                df_here,
+                column_config=style_dict
+                )
+    else:
+        for outcome in ['mrs_shift', 'mrs_0-2', 'utility', 'utility_shift']:
+            df_here = utils.take_subset_by_column_level_values(
+                df_results, outcome=[outcome])
+            df_here = utils.convert_row_to_table(
+                df_here, ['occlusion', 'treatment'])
+            st.markdown(f'### {outcome}')
+            style_dict = results.make_column_style_dict(
+                df_here.columns, format='%.3f')
+            st.dataframe(
+                df_here,
+                column_config=style_dict
+                )
 
 # ----- The end! -----
