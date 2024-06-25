f26d46e9e93321c7dfd7e9c6715b3f44ab9a5d4d
Anna Laws <a.laws2@exeter.ac.uk>
2024-06-25 07:44:14 +0100

    Move fragment contents to single container

--- pages/2_OPTIMIST_map.py
+++ pages/2_OPTIMIST_map.py
@@ -334,8 +334,7 @@ with container_results_tables:
 @st.experimental_fragment
 def display_mrs_dists():
     # User input:
-    with container_input_mrs_region:
-        bar_option = st.selectbox('Region for mRS distributions', bar_options)
+    bar_option = st.selectbox('Region for mRS distributions', bar_options)
 
     mrs_lists_dict, region_selected, col_pretty = (
         mrs.setup_for_mrs_dist_bars(
@@ -346,12 +345,12 @@ def display_mrs_dists():
             scenarios=scenario_mrs
             ))
 
-    with container_mrs_dists:
-        mrs.plot_mrs_bars(
-            mrs_lists_dict, title_text=f'{region_selected}<br>{col_pretty}')
+    mrs.plot_mrs_bars(
+        mrs_lists_dict, title_text=f'{region_selected}<br>{col_pretty}')
 
 
-display_mrs_dists()
+with container_mrs_dists:
+    display_mrs_dists()
 
 
 # ####################################
